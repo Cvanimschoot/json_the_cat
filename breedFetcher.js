9e5ab84b8ba992e9b9017f5167b1e736ce515a20
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-10-22 20:59:38 -0600

    Fixed some edge cases. #1: if the breed is not found, a message will be printed to the console to let the user know. #2: check for errors and make it not proceed further if an error occured. If error occurs, the error is printed to the console for the user. It should be hard for users to reach this as the url itself should not be able to be accessed. Instead they only have access to the query prompt and thus it should only ever really return that the breed is not found.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,16 @@
 const catType = process.argv[2];
 const request = require('request');
 const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + catType;
-console.log(url);
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
+  if (error) { // check for and print out the error if one occured.
+    console.log('error:', error);
+  } else { // if no error is found then we can continue
+    const data = JSON.parse(body); // Must parse data first to check if any data is stored within the object
+    if (data.length < 1) { // Check if there is any data. No data = Cat not found.
+      console.log('Breed not found. Please check spelling and try again.');
+    } else { // Otherwise if data is found print out the description.
+      console.log(data[0].description);
+    }
   }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-  console.log(typeof data);
 });
